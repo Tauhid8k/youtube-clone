9eb0b9dfae0b60f30f65b97e603dcd7d6e82a25b
Tauhid8k <tauhid8k@gmail.com>
2022-09-14 19:56:34 +0600

    navbar & search input added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,25 @@
+import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
+import SearchBar from './SearchBar';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Stack
+      py={1}
+      px={2}
+      direction='row'
+      alignItems='center'
+      gap='10px'
+      justifyContent='space-between'
+      className='shadow'
+      sx={{ backgroundColor: 'white' }}
+    >
+      <Link to='/'>
+        <img src='/img/logo.png' alt='logo' height='40' />
+      </Link>
+      <SearchBar />
+    </Stack>
+  );
 };
 
 export default Navbar;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,24 @@
+import { IconButton, Paper } from '@mui/material';
+import { Search } from '@mui/icons-material';
+
+const SearchBar = () => {
+  return (
+    <Paper
+      component='form'
+      onSubmit={() => {}}
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        boxShadow: 'none',
+        border: '1.8px solid #dbdbdb',
+      }}
+    >
+      <input type='text' className='search-input' placeholder='Search...' />
+      <IconButton type='submit' sx={{ color: 'red', borderRadius: '0' }}>
+        <Search />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
